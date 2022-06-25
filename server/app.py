0c21e0a3782f9c73cc48d1eb036c05fd74c121b3
kanancastroo <kcs.kanan@gmail.com>
2022-06-25 20:02:21 -0300

    now?

--- server/app.py
+++ server/app.py
@@ -1,6 +1,14 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from app import app, db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     db.create_all()
     app.run(host='0.0.0.0', port=port, debub=True)
+    manager.run()
