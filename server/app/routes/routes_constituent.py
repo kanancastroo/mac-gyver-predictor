4a51ba28ccf494ba2918bc945d1d9b287fab8460
kanancastroo <kcs.kanan@gmail.com>
2022-08-13 13:40:50 -0300

    new routes for sos and constituents

--- server/app/routes/routes_constituent.py
+++ server/app/routes/routes_constituent.py
@@ -1,6 +1,8 @@
 from app import app, db
 from flask import jsonify, request
-from models import Constituent
+from models import Constituent, Constituent_Basic_Feature, Basic_Feature, Basic_Feature_Emergent_Behavior, Emergent_Behavior
+import jsons
+import numpy as np
 
 @app.route("/add_constituent")
 def add_constituent():
@@ -34,3 +36,54 @@ def delete_constituent():
         return "Constituent id={} was deleted sucessfully".format(constituent_id)
     except Exception as e:
 	    return(str(e))   
+
+@app.route("/list_emergent_behaviors_from_constituents")
+def list_emergent_behaviors_from_constituents():
+    class EmergentBehavior():
+        def __init__(self, emergent_id, description):
+            self.emergent_id = emergent_id
+            self.description = description
+
+        def toJSON(self):
+            return jsons.dump(self)
+
+    constituent_id=request.args.get('constituent_id')
+    aux = constituent_id[1:-1]
+    arr = aux.split(sep=',')
+    # print(arr)
+    constituent_list = np.array(arr)
+    # for i in range(len(test)):
+    #     print(i, ' IS => ', test[i])
+    # test = arr.array('i', test1)
+    # print(test)
+    try:
+        results = db.session.query(Constituent.Constituent, Constituent_Basic_Feature.Constituent_Basic_Feature, \
+        Basic_Feature.Basic_Feature, Basic_Feature_Emergent_Behavior.Basic_Feature_Emergent_Behavior, \
+        Emergent_Behavior.Emergent_Behavior) \
+        .select_from(Constituent.Constituent) \
+        .join(Constituent_Basic_Feature.Constituent_Basic_Feature, Constituent.Constituent.constituent_id == Constituent_Basic_Feature.Constituent_Basic_Feature.constituent_id) \
+        .join(Basic_Feature.Basic_Feature, Constituent_Basic_Feature.Constituent_Basic_Feature.basic_feature_id == Basic_Feature.Basic_Feature.feature_id) \
+        .join(Basic_Feature_Emergent_Behavior.Basic_Feature_Emergent_Behavior, Basic_Feature.Basic_Feature.feature_id == Basic_Feature_Emergent_Behavior.Basic_Feature_Emergent_Behavior.basic_feature_id) \
+        .join(Emergent_Behavior.Emergent_Behavior, Basic_Feature_Emergent_Behavior.Basic_Feature_Emergent_Behavior.emergent_behavior_id == Emergent_Behavior.Emergent_Behavior.emergent_id) \
+        .filter(Constituent.Constituent.constituent_id.in_((constituent_list))) \
+        .distinct().all()
+
+        array_emergent_behaviors = []
+        print(results)
+
+        for constituent_id, relation_id, feature_id, relation_id, emergent_id in results:
+            emergent_behavior = EmergentBehavior(emergent_id=emergent_id.emergent_id, description=emergent_id.description)
+            array_emergent_behaviors.append(emergent_behavior)
+            # print(sos_id.sos_name, constituent_id.constituent_id, constituent_id.constituent_name)
+            # print(array_constituents)
+
+        seen_ids = set()
+        new_list = []
+        for obj in array_emergent_behaviors:
+            if obj.emergent_id not in seen_ids:
+                new_list.append(obj)
+                seen_ids.add(obj.emergent_id)
+
+        return jsonify([e.toJSON() for e in new_list])
+    except Exception as e:
+	    return(str(e))  
--- server/app/routes/routes_sos.py
+++ server/app/routes/routes_sos.py
@@ -1,6 +1,10 @@
 from app import app, db
 from flask import jsonify, request
-from models import SoS
+from models import SoS, SoS_Constituent, Constituent
+from sqlalchemy import update
+import jsons
+import numpy as np
+import array as arr
 
 @app.route("/add_sos")
 def add_sos():
@@ -13,16 +17,37 @@ def add_sos():
         db.session.commit()
         return "SoS added. sos_id={}".format(sos.sos_id)
     except Exception as e:
-	    return(str(e))    
-        
+	    return(str(e))
+
 @app.route("/list_sos")
 def list_sos():
+    sos_id=request.args.get('sos_id')
+    try:
+        sos = SoS.SoS.query.filter_by(sos_id=sos_id).first()
+        return jsonify(sos.sos_name)
+    except Exception as e:
+	    return(str(e))           
+
+@app.route("/list_all_sos")
+def list_all_sos():
     try:
         sos=SoS.SoS.query.all()
         return  jsonify([e.serialize() for e in sos])
         #return jsonify('HELL YEAH!')
     except Exception as e:
-	    return(str(e))
+	    return(str(e))          
+
+@app.route("/update_sos")
+def update_sos():
+    sos_name=request.args.get('sos_name')
+    sos_id=request.args.get('sos_id')
+    try:
+        sos = SoS.SoS.query.filter_by(sos_id=sos_id).first()
+        sos.sos_name = sos_name
+        db.session.commit()
+        return "SoS updated. sos_id={}".format(sos.sos_id)
+    except Exception as e:
+	    return(str(e))                       
 
 @app.route("/delete_sos")
 def delete_sos():
@@ -33,7 +58,45 @@ def delete_sos():
         db.session.commit()
         return "SoS id={} was deleted sucessfully".format(sos_id)
     except Exception as e:
-	    return(str(e))        
+	    return(str(e))       
+
+
+@app.route("/list_constituents_from_sos")
+def list_constituents_from_sos():
+    class CS():
+        def __init__(self, constituent_id, constituent_name):
+            self.constituent_id = constituent_id
+            self.constituent_name = constituent_name
+
+        def toJSON(self):
+            return jsons.dump(self)
+
+    sos_id=request.args.get('sos_id')
+    aux = sos_id[1:-1]
+    arr = aux.split(sep=',')
+    # print(arr)
+    sos_list = np.array(arr)
+    # for i in range(len(test)):
+    #     print(i, ' IS => ', test[i])
+    # test = arr.array('i', test1)
+    # print(test)
+    try:
+        results = db.session.query(SoS.SoS, SoS_Constituent.SoS_Constituent, Constituent.Constituent) \
+        .select_from(SoS.SoS) \
+        .join(SoS_Constituent.SoS_Constituent, SoS.SoS.sos_id == SoS_Constituent.SoS_Constituent.sos_id) \
+        .join(Constituent.Constituent, SoS_Constituent.SoS_Constituent.constituent_id == Constituent.Constituent.constituent_id) \
+        .filter(SoS.SoS.sos_id.in_((sos_list))).all()
+
+        array_constituents = []
+
+        for sos_id, relation_id, constituent_id in results:
+            constituent = CS(constituent_id=constituent_id.constituent_id, constituent_name=constituent_id.constituent_name)
+            array_constituents.append(constituent)
+            #print(sos_id.sos_name, constituent_id.constituent_id, constituent_id.constituent_name)
+            #print(array_constituents)
+        return jsonify([e.toJSON() for e in array_constituents])
+    except Exception as e:
+	    return(str(e))           
 
 
     
