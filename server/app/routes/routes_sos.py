e13ec88299f0c1a56a2c43bf63d1a949c01c32a3
kanancastroo <kcs.kanan@gmail.com>
2022-11-02 19:04:30 -0300

    refining CRUD and working on uuid stuffs

--- server/app/routes/routes_sos.py
+++ server/app/routes/routes_sos.py
@@ -5,77 +5,83 @@ from sqlalchemy import update
 import jsons
 import numpy as np
 import array as arr
+import uuid
 
-@app.route("/add_sos")
-def add_sos():
-    sos_name=request.args.get('sos_name')
-    try:
-        sos=SoS.SoS(
-            sos_name=sos_name,
-        )
-        db.session.add(sos)
-        db.session.commit()
-        return "SoS added. sos_id={}".format(sos.sos_id)
-    except Exception as e:
-	    return(str(e))
-
-@app.route("/list_sos")
-def list_sos():
-    sos_id=request.args.get('sos_id')
-    try:
-        sos = SoS.SoS.query.filter_by(sos_id=sos_id).first()
-        return jsonify(sos.sos_name)
-    except Exception as e:
-	    return(str(e))           
-
-@app.route("/list_all_sos")
-def list_all_sos():
+@app.route("/sos/get")
+def getAllSoS():
     try:
         sos=SoS.SoS.query.all()
         return  jsonify([e.serialize() for e in sos])
-        #return jsonify('HELL YEAH!')
     except Exception as e:
-	    return(str(e))          
+	    return(str(e))   
 
-@app.route("/update_sos")
-def update_sos():
+@app.route("/sos/add")
+def addSoS():
     sos_name=request.args.get('sos_name')
-    sos_id=request.args.get('sos_id')
     try:
-        sos = SoS.SoS.query.filter_by(sos_id=sos_id).first()
+        sos=SoS.SoS(
+            sos_external_id = uuid.uuid4().hex,
+            sos_name = sos_name
+        )
+        db.session.add(sos)
+        db.session.commit()
+        return "SoS added. sos_external_id={}".format(sos.sos_external_id)
+    except Exception as e:
+	    return(str(e))  
+
+@app.route("/sos/<sos_id>/get")
+def getSoS(sos_id):
+    sos_external_id=str(sos_id)
+    try:
+        sos = SoS.SoS.query.filter_by(sos_external_id=sos_external_id).first()
+        return jsonify(sos.sos_name)
+    except Exception as e:
+	    return(str(e))                
+
+@app.route("/sos/<sos_id>/update")
+def updateSoS(sos_id):
+    sos_external_id=str(sos_id)
+    sos_name=request.args.get('sos_name')
+    try:
+        sos = SoS.SoS.query.filter_by(sos_external_id=sos_external_id).first()
         sos.sos_name = sos_name
         db.session.commit()
-        return "SoS updated. sos_id={}".format(sos.sos_id)
+        return "SoS updated. sos_external_id={}".format(sos.sos_external_id)
     except Exception as e:
 	    return(str(e))                       
 
-@app.route("/delete_sos")
-def delete_sos():
-    sos_id=request.args.get('sos_id')
+@app.route("/sos/<sos_id>/delete")
+def deleteSoS(sos_id):
+    sos_external_id=str(sos_id)
     try:
-        sos = SoS.SoS.query.filter_by(sos_id=sos_id).one()
+        sos = SoS.SoS.query.filter_by(sos_external_id=sos_external_id).one()
         db.session.delete(sos)
         db.session.commit()
-        return "SoS id={} was deleted sucessfully".format(sos_id)
+        return "SoS id={} was deleted sucessfully".format(sos_external_id)
     except Exception as e:
 	    return(str(e))       
 
 
-@app.route("/list_constituents_from_sos")
-def list_constituents_from_sos():
+@app.route("/sos/<sos_id>/constituents/get")
+def getConstituentsFromSoS(sos_id):
     class CS():
-        def __init__(self, constituent_id, constituent_name):
-            self.constituent_id = constituent_id
+        def __init__(self, constituent_external_id, constituent_name):
+            # self.constituent_id = constituent_id
+            self.constituent_external_id = constituent_external_id
             self.constituent_name = constituent_name
 
         def toJSON(self):
             return jsons.dump(self)
 
-    sos_id=request.args.get('sos_id')
-    aux = sos_id[1:-1]
-    arr = aux.split(sep=',')
+    sos_external_id=str(sos_id)
+    sos = SoS.SoS.query.filter_by(sos_external_id=sos_external_id).one()
+
+    sos_id=sos.sos_id
+    # print('internal ID iS =>>> ', sos_id)
+    # aux = sos_id[1:-1]
+    # arr = aux.split(sep=',')
     # print(arr)
-    sos_list = np.array(arr)
+    # sos_list = np.array(arr)
     # for i in range(len(test)):
     #     print(i, ' IS => ', test[i])
     # test = arr.array('i', test1)
@@ -85,12 +91,13 @@ def list_constituents_from_sos():
         .select_from(SoS.SoS) \
         .join(SoS_Constituent.SoS_Constituent, SoS.SoS.sos_id == SoS_Constituent.SoS_Constituent.sos_id) \
         .join(Constituent.Constituent, SoS_Constituent.SoS_Constituent.constituent_id == Constituent.Constituent.constituent_id) \
-        .filter(SoS.SoS.sos_id.in_((sos_list))).all()
+        .filter(SoS.SoS.sos_id == sos_id).all()
+        # .filter(SoS.SoS.sos_id.in_((sos_list))).all()
 
         array_constituents = []
 
         for sos_id, relation_id, constituent_id in results:
-            constituent = CS(constituent_id=constituent_id.constituent_id, constituent_name=constituent_id.constituent_name)
+            constituent = CS(constituent_external_id=constituent_id.constituent_external_id, constituent_name=constituent_id.constituent_name)
             array_constituents.append(constituent)
             #print(sos_id.sos_name, constituent_id.constituent_id, constituent_id.constituent_name)
             #print(array_constituents)
